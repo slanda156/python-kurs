e7456b8ed33a26782c8ca7f629d1ecf4ce85ff5e
Christoph Heil <christoph.heil156@gmail.com>
2025-06-16 14:23:00 +0200

    Add initial implementation of main.py and TODO list

---
+++ 01. VSCode Install/hello-world/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def devide(a: int, b: float) -> float: #TODO: Test implementation
+    """_summary_
+
+    Arguments:
+        a -- _description_
+        b -- _description_
+
+    Returns:
+        _description_
+    """
+    return a / b
+
+
+def main() -> None:
+    # TODO
+    # * Important
+    # ! Deprecated
+    # ? Question
+    print("Hello, World!")
+    print(devide(1, 2.0))
+
+if __name__ == "__main__":
+    main()
